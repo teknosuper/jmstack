c44e98c443f1385d6be39e4ba1c174854f49db96
teknosuper <ponyow@gmail.com>
2023-09-11 02:45:18 +0700

    Fix view song bug

--- html/mp3online/protected/controllers/api/SongShowAction.php
+++ html/mp3online/protected/controllers/api/SongShowAction.php
@@ -137,115 +137,78 @@ class SongShowAction extends CAction
         else
         {
             $item = Song::model()->findByPk($id);
-            if(count($item)>0)
-            {
-                $path= Yii::app()->getBaseUrl(true);
-                if(strlen($item->link)>0)
-                {
-                    if(substr_count($item->link,'http') > 0 )
-                    {
-                        //echo 123;
+            if (isset($item)) {
+                $path = Yii::app()->getBaseUrl(true);
+
+                // Process the 'link' attribute
+                if (strlen($item->link) > 0) {
+                    if (substr_count($item->link, 'http') > 0) {
                         $url_music = $item->link;
+                    } else {
+                        $url_music = $path . '/upload/' . $item->link;
                     }
-                    else
-                    {
-                        //echo 456;exit;
-                        $url_music = $path.'/upload/'.$item->link;
-                    }
-                }
-                else
-                {
+                } else {
                     $url_music = '';
                 }
 
-                if(strlen($item->image)>0)
-                {
-                    $image= $path.'/images/song/'.$item->image;
-                }
-                else
-                {
-                    $image=$path.'/images/www/no_image.jpg';
+                // Process the 'image' attribute
+                if (strlen($item->image) > 0) {
+                    $image = $path . '/images/song/' . $item->image;
+                } else {
+                    $image = $path . '/images/www/no_image.jpg';
                 }
 
-                if(strlen($item->song_name)>0)
-                    $songname =$item->song_name ;
-                else
-                    $songname = '';
-                if(strlen($item->lyrics)>0)
-                    $lyrics =$item->lyrics ;
-                else
-                    $lyrics = '';
-                if(strlen($item->album_id)>0)
-                    $album =$item->album_id ;
-                else
-                    $album = '';
-                if(strlen($item->author_id)>0)
-                    $author =$item->author_id ;
-                else
-                    $author = '';
-                if(strlen($item->category_id)>0)
-                    $cate =$item->category_id ;
-                else
-                    $cate = '';
-                if(strlen($item->link_app)>0)
-                    $linkapp =$item->link_app ;
-                else
-                    $linkapp = '';
+                // Process other attributes
+                $songname = strlen($item->song_name) > 0 ? $item->song_name : '';
+                $lyrics = strlen($item->lyrics) > 0 ? $item->lyrics : '';
+                $album = strlen($item->album_id) > 0 ? $item->album_id : '';
+                $author = strlen($item->author_id) > 0 ? $item->author_id : '';
+                $cate = strlen($item->category_id) > 0 ? $item->category_id : '';
+                $linkapp = strlen($item->link_app) > 0 ? $item->link_app : '';
 
-                $sing= Singer::model()->findByPk($item->singer_id);
-                if(count($sing)>0)
-                    $name=$sing->singer_name;
-                else
-                    $name= '';
+                $sing = Singer::model()->findByPk($item->singer_id);
+                $name = isset($sing) ? $sing->singer_name : '';
 
-                $tran = Translattions::model()->find(" model_id =".$item->song_id,"and table_name = 'song' and attribute = 'song_name' ");
-                //var_dump($tran);exit;
-                if(count($tran)>0)
-                {
-                    $name_vi = $tran->value;
-                }
-                else
-                    $name_vi = '';
+                $tran = Translattions::model()->find("model_id = " . $item->song_id . " and table_name = 'song' and attribute = 'song_name'");
+                $name_vi = isset($tran) ? $tran->value : '';
 
-                $trans = Translattions::model()->find(" model_id =".$item->song_id,"and table_name = 'song' and attribute = 'lyrics' ");
-                //var_dump($trans);exit;
-                if(count($trans)>0)
-                {
-                    $lyrics_vi = $trans->value;
-                }
-                else
-                    $lyrics_vi = '';
+                $trans = Translattions::model()->find("model_id = " . $item->song_id . " and table_name = 'song' and attribute = 'lyrics'");
+                $lyrics_vi = isset($trans) ? $trans->value : '';
 
+                // Construct the data array
                 $data = array(
-                    'id'=>$item->song_id,
-                    'name'=>$songname,
-                    'name_vi'=>$name_vi,
-                    'lyrics'=>$lyrics,
-                    'lyrics_vi'=>$lyrics_vi,
-                    'link'=>$url_music,
-                    'singerName'=>$name,
-                    'listen'=>$item->listen,
-                    'download'=>$item->download,
-                    'album_id'=>$album,
-                    'author_id'=>$author,
-                    'category_id'=>$cate,
-                    'link_app'=>$linkapp,
-                    'image'=>$image,
-                    'description'=>isset($item->description) ? $item->description : '',
+                    'id' => $item->song_id,
+                    'name' => $songname,
+                    'name_vi' => $name_vi,
+                    'lyrics' => $lyrics,
+                    'lyrics_vi' => $lyrics_vi,
+                    'link' => $url_music,
+                    'singerName' => $name,
+                    'listen' => $item->listen,
+                    'download' => $item->download,
+                    'album_id' => $album,
+                    'author_id' => $author,
+                    'category_id' => $cate,
+                    'link_app' => $linkapp,
+                    'image' => $image,
+                    'description' => isset($item->description) ? $item->description : '',
                 );
 
+                // Send a JSON response
                 ApiController::sendResponse(200, CJSON::encode(array(
                     'status' => 'SUCCESS',
                     'data' => $data,
-                    'message' => 'OK',)));
-            }
-            else
-            {
+                    'message' => 'OK',
+                )));
+            } else {
+                // Send an empty JSON response
                 ApiController::sendResponse(200, CJSON::encode(array(
                     'status' => 'SUCCESS',
                     'data' => array(),
-                    'message' => 'OK',)));
+                    'message' => 'OK',
+                )));
             }
+
         }
     }
 }
